43c342498a608eecfdc85b6043ccdd9f1f803a8f
oldschoolroyale <8224460Shox>
2022-03-20 00:58:17 +0500

    test

--- src/main/java/com/brm/inomhealthcare/controller/HomeController.java
+++ src/main/java/com/brm/inomhealthcare/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.brm.inomhealthcare.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getHome(){
         return "index";
     }
