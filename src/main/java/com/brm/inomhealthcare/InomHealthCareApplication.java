0a492ebe02f652bae6d2641d3de6c3ff4972a919
oldschoolroyale <8224460Shox>
2022-03-20 00:22:35 +0500

    First commit

---
+++ src/main/java/com/brm/inomhealthcare/InomHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.brm.inomhealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InomHealthCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InomHealthCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brm/inomhealthcare/InomHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brm.inomhealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InomHealthCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
