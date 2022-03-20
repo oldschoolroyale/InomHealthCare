87c474a4b1a659e376521e32a4c99bf59c942d41
oldschoolroyale <8224460Shox>
2022-03-20 13:07:42 +0500

    test

--- src/main/java/com/brm/inomhealthcare/InomHealthCareApplication.java
+++ src/main/java/com/brm/inomhealthcare/InomHealthCareApplication.java
@@ -1,11 +1,12 @@
 package com.brm.inomhealthcare;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class InomHealthCareApplication {
 
     public static void main(String[] args) {
